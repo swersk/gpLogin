92746735deceb768855db0f9d56909c5f048b637
Lauren Swersky <lswersky@greenpeace.org>
2024-11-04 14:35:37 +0100

    fix: automation.js

--- automation.js
+++ automation.js
@@ -2,7 +2,7 @@ import { chromium } from 'playwright';
 
 (async () => {
   // Launch the browser in UI mode (headless: false to see it in action)
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
 
   const context = await browser.newContext();
   const page = await context.newPage();
