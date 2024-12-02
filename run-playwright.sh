ade8a8e70d753c323b398778b9abdd8695b28a37
Lauren Swersky <lswersky@greenpeace.org>
2024-12-02 11:37:17 +0100

    refactor to be able to manually trigger

--- run-playwright.sh
+++ run-playwright.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Define holidays
 HOLIDAYS=("11-01" "12-06" "12-23" "12-24" "12-25" "12-26" "12-27" "12-28" "12-29" "12-30" "12-31" "01-01")
 
 # Get today's date in MM-DD format
@@ -11,16 +12,12 @@ if [[ " ${HOLIDAYS[@]} " =~ " ${TODAY} " ]]; then
   exit 0
 fi
 
-# Check if an action is provided (clock-in or clock-out)
+# Get action from argument
 ACTION=$1
-if [[ -z "$ACTION" ]]; then
-  echo "No action provided! Please specify 'clock-in' or 'clock-out'."
-  exit 1
-fi
 
 # Validate the action
-if [[ "$ACTION" != "clock-in" && "$ACTION" != "clock-out" ]]; then
-  echo "Invalid action: $ACTION. Use 'clock-in' or 'clock-out'."
+if [[ -z "$ACTION" || ( "$ACTION" != "clock-in" && "$ACTION" != "clock-out" ) ]]; then
+  echo "Invalid or missing action. Use 'clock-in' or 'clock-out'."
   exit 1
 fi
 
