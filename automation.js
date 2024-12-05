43f8739330c4eb19562e34fd138b72c7fdeb37d1
Lauren Swersky <lswersky@greenpeace.org>
2024-12-05 12:23:54 +0100

    fix: GitHub secrets to run Actions

--- automation.js
+++ automation.js
@@ -14,14 +14,15 @@ if (!['clock-in', 'clock-out'].includes(action)) {
   const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
-
   const url = 'https://w3.cezanneondemand.com/CezanneOnDemand/-/GreenpeaceEspana/Account/LogIn';
-  await page.goto(url);
-  await page.fill('input[name="Username"]', process.env.USERNAME);
-  await page.fill('#Password', process.env.PASSWORD);
-  await page.click('#login-form > form > div:nth-child(5) > button');
 
   if (action === 'clock-in') {
+    await page.goto(url);
+    await page.screenshot({ path: './screenshots/01-url.png' });
+    await page.fill('input[name="Username"]', process.env.USERNAME);
+    await page.fill('#Password', process.env.PASSWORD);
+    await page.screenshot({ path: './screenshots/02-login-info.png' });
+    await page.click('#login-form > form > div:nth-child(5) > button');
     console.log('Clock-in process initiated...');
     await page.click('button.cz-clock-in-button-dot.cz-clock-in-button-green');
     await page.screenshot({ path: './screenshots/05-clock-in-clicked.png' });
@@ -30,6 +31,12 @@ if (!['clock-in', 'clock-out'].includes(action)) {
     await page.screenshot({ path: './screenshots/06-accept-clicked.png' });
     console.log('✅ Logged in!');
   } else if (action === 'clock-out') {
+    await page.goto(url);
+    await page.screenshot({ path: './screenshots/01-url.png' });
+    await page.fill('input[name="Username"]', process.env.USERNAME);
+    await page.fill('#Password', process.env.PASSWORD);
+    await page.screenshot({ path: './screenshots/02-login-info.png' });
+    await page.click('#login-form > form > div:nth-child(5) > button');
     console.log('Clock-out process initiated...');
     await page.click('button.cz-clock-in-button-dot.cz-clock-in-button-blue');
     await page.screenshot({ path: './screenshots/06-clock-out-clicked.png' });
