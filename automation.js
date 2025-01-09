c6048b694837bd954ce562b68a1892c8b0d0b63a
Lauren Swersky <lswersky@greenpeace.org>
2025-01-08 20:23:56 -0500

    feat: maxBounds

--- automation.js
+++ automation.js
@@ -29,11 +29,11 @@ if (!['clock-in', 'clock-out'].includes(action)) {
     await page.click('#login-form > form > div:nth-child(5) > button');
     console.log('Clock-in process initiated...');
     await page.click('button.cz-clock-in-button-dot.cz-clock-in-button-green');
-    await page.screenshot({ path: './screenshots/05-clock-in-clicked.png' });
+    await page.screenshot({ path: './screenshots/03-clock-in-clicked.png' });
     console.log('Clock-in clicked');
     await page.click('button.cz-primary-button:has-text("Aceptar")');
-    await page.screenshot({ path: './screenshots/06-accept-clicked.png' });
-    console.log('✅ Accept button clicked'); 
+    await page.screenshot({ path: './screenshots/04-accept-clicked.png' });
+    console.log('Confirmed clock-in.'); 
     const clockOutLabel = page.locator('strong.cz-clock-in-out-label', { hasText: 'Registro de la hora de salida' });
     console.log("Waiting for the 'Clock-out' label to be visible...");
     try {
@@ -46,31 +46,30 @@ if (!['clock-in', 'clock-out'].includes(action)) {
       process.exit(1);  
     }
   } else if (action === 'clock-out') {
-    console.log("Logging out...");
+    console.log("[1] Clocking out...");
     await page.goto(url);
-    await page.screenshot({ path: './screenshots/01-url.png' });
+    await page.screenshot({ path: './screenshots/08-url.png' });
     const username = process.env.USERNAME
     if (!username) {
-      console.error('Username is undefined.');
+      console.error('Username is undefined. Cannot proceed with clock-out.');
       process.exit(1);
     }
     await page.fill('input[name="Username"]', process.env.USERNAME);
     console.log("Username entered with username: ", process.env.USERNAME);
     await page.fill('#Password', process.env.PASSWORD);
     console.log("Password. entered.");
-    await page.screenshot({ path: './screenshots/02-login-info.png' });
+    await page.screenshot({ path: './screenshots/09-login-info.png' });
     await page.click('#login-form > form > div:nth-child(5) > button');
-    console.log('Clock-out process initiated...');
     await page.click('button.cz-clock-in-button-dot.cz-clock-in-button-blue');
-    await page.screenshot({ path: './screenshots/06-clock-out-clicked.png' });
-    console.log('Clock-out clicked');
+    await page.screenshot({ path: './screenshots/10-clock-out-clicked.png' });
+    console.log('Clock-out clicked.');
     await page.click('button.cz-primary-button:has-text("Aceptar")');
-    // const clockInLabel = page.locator('strong.cz-clock-in-out-label', { hasText: 'Registro de la hora de llegada' });
-    // await clockInLabel.isVisible();
-    // if (!clockInLabel) {
-    //   console.error('Clock-in label not found.');
-    // }
-    await clockInLabel.screenshot({ path: './screenshots/07-clock-out-label-visible.png' });
+    const clockInLabel = page.locator('strong.cz-clock-in-out-label', { hasText: 'Registro de la hora de llegada' });
+    await clockInLabel.isVisible();
+    if (!clockInLabel) {
+      console.error('Clock-in label not found.');
+    }
+    await clockInLabel.screenshot({ path: './screenshots/11-clock-out-label-visible.png' });
     console.log('✅ Logged out!');
   }
   await browser.close();
